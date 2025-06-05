0fe91b9039475cba721ff04be895033d97a060f1
NMoreira25 <nathalia.moreira74@icloud.com>
2025-06-05 09:27:54 -0300

    Add files via upload

---
+++ nathalia.py
@@ -0,0 +1,6 @@
+preco_original = 100
+valor_desconto = 20 
+valor_desconto = preco_original *(valor_desconto / 100)
+preco_final = preco_original - valor_desconto 
+print(preco_final)
+print(valor_desconto)
